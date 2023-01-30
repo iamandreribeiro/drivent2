516cbe133ef8c057366e4dd5eb010a0f5a2eb233
iamandreribeiro <andreribeiroorafael@gmail.com>
2023-01-30 17:53:13 -0300

    errors

--- tests copy/factories/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./users-factory";
-export * from "./events-factory";
-export * from "./sessions-factory";
-export * from "./enrollments-factory";
-export * from "./tickets-factory";
-export * from "./payments-factory";
